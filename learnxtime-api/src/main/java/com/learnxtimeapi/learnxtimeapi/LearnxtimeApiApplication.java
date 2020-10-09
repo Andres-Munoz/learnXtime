f1bcac6c93a8617da54bfa7251cd1a15b7d4a2ca
Andres Muñoz <munooz.andres@gmail.com>
2020-10-09 15:16:55 -0500

    Creación proyecto base

---
+++ learnxtime-api/src/main/java/com/learnxtimeapi/learnxtimeapi/LearnxtimeApiApplication.java
@@ -0,0 +1,13 @@
+package com.learnxtimeapi.learnxtimeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnxtimeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnxtimeApiApplication.class, args);
+	}
+
+}
---
+++ learnxtime-api/src/test/java/com/learnxtimeapi/learnxtimeapi/LearnxtimeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnxtimeapi.learnxtimeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnxtimeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
